4248310285365920be7d49dccd839fabe0de2945
onikarubi <printuserselect@gmial.com>
2021-07-17 12:27:52 +0900

    スマートフォン版のfooter表示を動的に変更。
    スマートフォン版でも動的に表示できるようにした。

--- footer.php
+++ footer.php
@@ -7,15 +7,7 @@
       </nav>
 
       <nav class="footer-menu is-sp">
-        <ul class="footer-menu__list">
-          <li class="footer-menu__item"><a href="./index.html" class="footer-menu__link">HOME</a></li>
-          <li class="footer-menu__item"><a href="./news.html" class="footer-menu__link">NEWS</a></li>
-          <li class="footer-menu__item"><a href="./service.html" class="footer-menu__link">SERVICE</a></li>
-          <li class="footer-menu__item"><a href="./works.html" class="footer-menu__link">WORKS</a></li>
-          <li class="footer-menu__item"><a href="./company.html" class="footer-menu__link">COMPANY</a></li>
-          <li class="footer-menu__item"><a href="./recruit.html" class="footer-menu__link">RECRUIT</a></li>
-          <li class="footer-menu__item"><a href="./contact.html" class="footer-menu__link">CONTACT</a></li>
-        </ul>
+        <?php global_nav_menu('footer-sp', 'footer'); ?>
       </nav>
 
       <div class="copyright">
